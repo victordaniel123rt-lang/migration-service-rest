0fcd858196fa25855f679520c2bd08318048a365
victord <victordaniel.123rt@gmail.com>
2026-08-19 09:41:36 -0600

    Commit

---
+++ src/main/java/com/vdgarcia/migration_service_rest/MigrationServiceRestApplication.java
@@ -0,0 +1,13 @@
+package com.vdgarcia.migration_service_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationServiceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationServiceRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vdgarcia/migration_service_rest/MigrationServiceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdgarcia.migration_service_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationServiceRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
